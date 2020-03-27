a491dde94060ce52d10b20307ff8069c0ac4a6bd
apisitprompha <apisitprompha@gmail.com>
2020-03-28 05:08:17 +0700

    ADD edit function

--- mvc_dotNetFramework_c_employee/Controllers/EmployeeController.cs
+++ mvc_dotNetFramework_c_employee/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,18 +24,42 @@ namespace mvc_dotNetFramework_c_employee.Controllers
         [HttpGet]
         public ActionResult AddOrEdit(int id = 0)
         {
+            if (id == 0) { 
             return View(new Employee());
+            }
+            using (DBModel db = new DBModel())
+            {
+                return View(db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>());
+            }
         }
 
         [HttpPost]
         public ActionResult AddOrEdit(Employee emp)
         {
             using (DBModel db = new DBModel())
+                if (emp.EmployeeID == 0) 
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
             {
-                db.Employees.Add(emp);
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
                 db.SaveChanges();
-                return Json(new { success=true, message="Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
             }
         }
     }
+
 }
